ceaf66071026286bfb71142db45f2823a09c270f
David Gordon <davgordo@redhat.com>
2024-02-12 01:47:30 -0500

    example legacy spring boot v2 service

---
+++ src/main/java/com/rh/demo/Application.java
@@ -0,0 +1,75 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.rh.demo;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Autowired
+    private Environment env;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @Component
+    class RestApi extends RouteBuilder {
+
+        @Override
+        public void configure() {
+            restConfiguration()
+                .contextPath("").apiContextPath("/api-doc")
+                    .apiProperty("api.title", "Camel REST API")
+                    .apiProperty("api.version", "1.0")
+                    .apiProperty("cors", "true")
+                    .apiContextRouteId("doc-api")
+                    .port(env.getProperty("server.port", "8080"))
+                .bindingMode(RestBindingMode.json);
+
+            rest("/orders").description("Orders REST service")
+                .get("{id}").description("Details of an order by id")
+                    .route().routeId("order-api")
+                    .bean(Database.class, "findOrder(${header.id})");
+        }
+    }
+
+    @Component
+    class Backend extends RouteBuilder {
+
+        @Override
+        public void configure() {
+            // A first route generates some orders and queue them in DB
+            from("timer:new-order?delay=1s&period={{example.generateOrderPeriod:2s}}")
+                .routeId("generate-order")
+                .log("Inserted new order step 1")
+                .bean("orderService", "generateOrder")
+                .log("Inserted new order step 2")
+                .to("jpa:org.apache.camel.example.spring.boot.rest.jpa.Order")
+                .log("Inserted new order ${body.id}");
+
+        }
+    }
+}
